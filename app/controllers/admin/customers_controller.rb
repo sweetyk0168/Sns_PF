3597ec14c8a831208e260b006e879dfa461735e6
EC2 Default User <ec2-user@ip-172-31-26-78.ap-northeast-1.compute.internal>
2023-10-06 11:34:41 +0000

    20231006_管理者管理機能実装中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,26 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
   layout 'admin_header'
-  def show
-  end
-
-  def edit
-  end
 
   def index
     @customers = Customer.page(params[:page]).per(10)
+  end
+
+  def show
+    @customers = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render 'edit'
+    end
   end
 
   private
