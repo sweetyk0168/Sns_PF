dd8e2bf97120b459c054b84b6d00e2fb18cf9d3c
EC2 Default User <ec2-user@ip-172-31-26-78.ap-northeast-1.compute.internal>
2023-11-16 12:10:49 +0000

    20231101014710 RemovePostFromPostEventReposマイグレーションファイル削除済み

--- db/migrate/20231101014710_remove_post_from_post_event_repos.rb
+++
@@ -1,6 +0,0 @@
-class RemovePostFromPostEventRepos < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :post_event_repos, :post, null: false, foreign_key: true
-    remove_reference :post_event_repos, :event_repo, null: false, foreign_key: true
-  end
-end
