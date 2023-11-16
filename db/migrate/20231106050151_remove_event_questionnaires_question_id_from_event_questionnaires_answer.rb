b277463fa998f18e8ef34ce7213ee2d8d5076a48
EC2 Default User <ec2-user@ip-172-31-26-78.ap-northeast-1.compute.internal>
2023-11-16 12:16:05 +0000

    マイグレーションファイル削除済み

--- db/migrate/20231106050151_remove_event_questionnaires_question_id_from_event_questionnaires_answer.rb
+++
@@ -1,6 +0,0 @@
-class RemoveEventQuestionnairesQuestionIdFromEventQuestionnairesAnswer < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :event_questionnaires_answers, :event_questionnaires_questions
-    remove_reference :event_questionnaires_answers, :event_questionnaires_question_id, index: true
-  end
-end
