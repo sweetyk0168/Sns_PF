1f4cce306e76ba087b373ba73fd5eb82badaab41
EC2 Default User <ec2-user@ip-172-31-26-78.ap-northeast-1.compute.internal>
2023-11-16 11:33:17 +0000

    customer_idカラムnullfalseに変更postテーブルマイグレーションファイル削除済

--- db/migrate/20231023114636_change_option_customerid_body_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeOptionCustomeridBodyToPosts < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :posts, :customer_id, false, ""
-    change_column_default :posts, :customer_id, from: nil, to: ""
-  end
-end
